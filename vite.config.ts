3957e79e16d73b25893343199ac10e731cc0b9b3
voluntas <nakai@shiguredo.jp>
2025-05-02 18:09:45 +0900

    build の vite 化

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import pkg from "./package.json";
+
+const banner = `/**
+ * ${pkg.name}
+ * ${pkg.description}
+ * @version: ${pkg.version}
+ * @author: ${pkg.author}
+ * @license: ${pkg.license}
+ **/
+`;
+
+export default defineConfig({
+	define: {
+		__RNNOISE_VERSION__: JSON.stringify(pkg.version),
+	},
+	root: process.cwd(),
+	build: {
+		minify: "esbuild",
+		target: "es2022",
+		emptyOutDir: true,
+		manifest: true,
+		outDir: resolve(__dirname, "./dist"),
+		lib: {
+			entry: resolve(__dirname, "src/rnnoise.ts"),
+			name: "RNNoise",
+			formats: ["es"],
+			fileName: "rnnoise",
+		},
+		rollupOptions: {
+			output: {
+				banner: banner,
+			},
+		},
+	},
+	envDir: resolve(__dirname, "./"),
+	plugins: [
+		dts({
+			include: ["src/**/*"],
+		}),
+	],
+});
