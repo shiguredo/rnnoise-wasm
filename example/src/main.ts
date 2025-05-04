150d12897023a1672fa9316aae93ae96852b32e2
voluntas <nakai@shiguredo.jp>
2025-05-04 09:29:36 +0900

    整理

--- example/src/main.ts
+++ example/src/main.ts
@@ -9,20 +9,27 @@ document.addEventListener('DOMContentLoaded', async () => {
   const assetsPath = '.'
   const processor = new NoiseSuppressionProcessor(assetsPath)
 
-  let audioCtx
-  let analyserOriginal
-  let analyserProcessed
-  let sourceOriginal
-  let sourceProcessed
+  let audioCtx: AudioContext | undefined
+  let analyserOriginal: AnalyserNode | undefined
+  let analyserProcessed: AnalyserNode | undefined
+  let sourceOriginal: MediaStreamAudioSourceNode | undefined
+  let sourceProcessed: MediaStreamAudioSourceNode | undefined
+
   function initAudioAnalysersIfNeed() {
     if (audioCtx === undefined) {
-      audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+      audioCtx = new window.AudioContext()
 
       analyserOriginal = audioCtx.createAnalyser()
-      visualize(analyserOriginal, document.getElementById('oscilloscopeOriginal'))
+      visualize(
+        analyserOriginal,
+        document.getElementById('oscilloscopeOriginal') as HTMLCanvasElement,
+      )
 
       analyserProcessed = audioCtx.createAnalyser()
-      visualize(analyserProcessed, document.getElementById('oscilloscopeProcessed'))
+      visualize(
+        analyserProcessed,
+        document.getElementById('oscilloscopeProcessed') as HTMLCanvasElement,
+      )
     }
   }
 
@@ -37,10 +44,16 @@ document.addEventListener('DOMContentLoaded', async () => {
     return navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
       initAudioAnalysersIfNeed()
 
-      if (sourceOriginal != undefined) {
+      if (sourceOriginal !== undefined) {
         sourceOriginal.disconnect()
       }
+      if (audioCtx === undefined) {
+        throw Error('audio context not found')
+      }
       sourceOriginal = audioCtx.createMediaStreamSource(stream)
+      if (analyserOriginal === undefined) {
+        throw Error('analyser not found')
+      }
       sourceOriginal.connect(analyserOriginal)
 
       return stream
@@ -50,11 +63,14 @@ document.addEventListener('DOMContentLoaded', async () => {
   function playOriginalAudio() {
     processor.stopProcessing()
 
-    const audioElement = document.getElementById('audio')
     getUserMedia().then((stream) => {
-      if (sourceProcessed != undefined) {
+      if (sourceProcessed !== undefined) {
         sourceProcessed.disconnect()
         sourceProcessed = undefined
+      }
+      const audioElement = document.querySelector<HTMLMediaElement>('#audio')
+      if (audioElement === null) {
+        throw Error('audio element not found')
       }
       audioElement.srcObject = stream
     })
@@ -63,25 +79,40 @@ document.addEventListener('DOMContentLoaded', async () => {
   function playProcessedAudio() {
     processor.stopProcessing()
 
-    const audioElement = document.getElementById('audio')
+    const audioElement = document.querySelector<HTMLMediaElement>('#audio')
+    if (audioElement === null) {
+      throw Error('audio element not found')
+    }
     getUserMedia().then((stream) => {
       const track = stream.getAudioTracks()[0]
       processor.startProcessing(track).then((processed_track) => {
         const stream = new MediaStream([processed_track])
 
-        if (sourceProcessed != undefined) {
+        if (sourceProcessed !== undefined) {
           sourceProcessed.disconnect()
         }
+        if (audioCtx === undefined) {
+          throw Error('audio context not found')
+        }
         sourceProcessed = audioCtx.createMediaStreamSource(stream)
+        if (analyserProcessed === undefined) {
+          throw Error('analyser not found')
+        }
         sourceProcessed.connect(analyserProcessed)
 
+        if (audioElement === null) {
+          throw Error('audio element not found')
+        }
         audioElement.srcObject = stream
       })
     })
   }
 
   function stopAudio() {
-    const audioElement = document.getElementById('audio')
+    const audioElement = document.querySelector<HTMLMediaElement>('#audio')
+    if (audioElement === null) {
+      throw Error('audio element not found')
+    }
     audioElement.pause()
 
     if (sourceOriginal !== undefined) {
@@ -94,7 +125,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
   }
 
-  function visualize(analyser, canvas) {
+  function visualize(analyser: AnalyserNode, canvas: HTMLCanvasElement) {
     requestAnimationFrame(() => {
       visualize(analyser, canvas)
     })
@@ -104,6 +135,9 @@ document.addEventListener('DOMContentLoaded', async () => {
     analyser.getByteTimeDomainData(dataArray)
 
     const canvasCtx = canvas.getContext('2d')
+    if (canvasCtx === null) {
+      throw Error('canvas context not found')
+    }
     canvasCtx.fillStyle = 'rgb(200, 200, 200)'
     canvasCtx.fillRect(0, 0, canvas.width, canvas.height)
 
@@ -139,16 +173,19 @@ document.addEventListener('DOMContentLoaded', async () => {
     canvasCtx.stroke()
   }
 
-  function clearCanvas(canvas) {
+  function clearCanvas(canvas: HTMLCanvasElement) {
     const canvasCtx = canvas.getContext('2d')
+    if (canvasCtx === null) {
+      throw Error('canvas context not found')
+    }
     canvasCtx.fillStyle = 'rgb(200, 200, 200)'
     canvasCtx.fillRect(0, 0, canvas.width, canvas.height)
   }
 
-  document.getElementById('playProcessedAudio').addEventListener('click', playProcessedAudio)
-  document.getElementById('playOriginalAudio').addEventListener('click', playOriginalAudio)
-  document.getElementById('stopAudio').addEventListener('click', stopAudio)
+  document.getElementById('playProcessedAudio')?.addEventListener('click', playProcessedAudio)
+  document.getElementById('playOriginalAudio')?.addEventListener('click', playOriginalAudio)
+  document.getElementById('stopAudio')?.addEventListener('click', stopAudio)
 
-  clearCanvas(document.getElementById('oscilloscopeOriginal'))
-  clearCanvas(document.getElementById('oscilloscopeProcessed'))
+  clearCanvas(document.getElementById('oscilloscopeOriginal') as HTMLCanvasElement)
+  clearCanvas(document.getElementById('oscilloscopeProcessed') as HTMLCanvasElement)
 })
